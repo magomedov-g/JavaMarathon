8d44edf670425bf75d11c6a0078b6e5cc14ee60b
magomedov-g <gazimagomedm054@gmail.com>
2024-04-15 15:53:20 +0300

    init commit

--- src/main/java/day0/Task1.java
+++ src/main/java/day0/Task1.java
@@ -1,5 +1,11 @@
 package day0;
 
 public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("said" + " фсбшник");
+//гьале код он работает я проверил щуйля лъе де
+        //коммитги пушги гьабе гьанже
+        // минуту
 
+    }
 }
