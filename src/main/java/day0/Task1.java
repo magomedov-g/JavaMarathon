9cd5e2dc7523a5e2d739a516d56b493148744609
magomedov-g <gazimagomedm054@gmail.com>
2024-04-15 16:04:05 +0300

    workspace.xml added

--- src/main/java/day0/Task1.java
+++ src/main/java/day0/Task1.java
@@ -3,9 +3,7 @@ package day0;
 public class Task1 {
     public static void main(String[] args) {
         System.out.println("said" + " фсбшник");
-//гьале код он работает я проверил щуйля лъе де
-        //коммитги пушги гьабе гьанже
-        // ебaть
+
 
     }
 }
